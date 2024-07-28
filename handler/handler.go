36d76c53eaf4d1ade459de00598ec91ecbcb648c
aidint <3762928+aidint@users.noreply.github.com>
2024-07-28 20:53:50 +0200

    complete chain handler

--- handler/handler.go
+++ handler/handler.go
@@ -10,45 +10,72 @@ const (
 	ChainHandlerType = iota
 )
 
+type HandlerState int
+
+const (
+	HandlerNotStarted = iota
+	HandlerError
+	HandlerSuccess
+)
+
 type Handler interface {
-	Execute(map[HandlerType]*Handler)
+	Execute(map[HandlerType]Handler)
 	GetType() HandlerType
-  ShouldContinue() bool
-  GetErrors() []error
+	ShouldProceed() bool
+	GetState() HandlerState
+	GetErrors() []error
 }
 
 type ChainHandler struct {
-  chain []*Handler
-  shouldContinue bool
-  errors []error
-  exeMap map[HandlerType]*Handler
-  Done bool
+	chain   []Handler
+	errors  []error
+	exeMap  map[HandlerType]Handler
+	state   HandlerState
+	proceed bool
 }
 
 func (c *ChainHandler) GetType() HandlerType {
-  return ChainHandlerType
+	return ChainHandlerType
 }
 
-func (c *ChainHandler) ShouldContinue() bool {
-  return c.shouldContinue
+func (c *ChainHandler) ShouldProceed() bool {
+	return c.proceed
+}
+
+func (c *ChainHandler) GetState() HandlerState {
+	return c.state
 }
 
 func (c *ChainHandler) GetErrors() []error {
-  return c.errors
+	return c.errors
 }
 
-func (c *ChainHandler) AddHandler(h *Handler) {
-  if c.Done {
-    log.Panic("Cannot add a handler to a chain that has already been executed.")
-  }
-  
-  c.chain = append(c.chain, h)
-  c.shouldContinue = true
+func (c *ChainHandler) AddHandler(h Handler) {
+	if c.state != HandlerNotStarted {
+		log.Panic("Cannot add a handler to a chain that has already been executed.")
+	}
+
+	c.chain = append(c.chain, h)
 }
 
-func (c *ChainHandler) Execute(state map[HandlerType]*Handler) {
-  if c.Done {
-    log.Panic("Cannot execute a chain that has already been executed.")
-  }
-  c.exeMap = state
+func (c *ChainHandler) Execute(state map[HandlerType]Handler) {
+	if c.state != HandlerNotStarted {
+		log.Panic("Cannot execute a chain that has already been executed.")
+	}
+
+	c.exeMap = state
+
+	for _, h := range c.chain {
+		h.Execute(c.exeMap)
+		if h.GetState() == HandlerError {
+			c.errors = append(c.errors, h.GetErrors()...)
+			if !h.ShouldProceed() {
+				c.proceed = false
+				c.state = HandlerError
+			}
+		}
+		c.exeMap[h.GetType()] = h
+	}
+	c.proceed = true
+	c.state = HandlerSuccess
 }
